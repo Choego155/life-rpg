11de14b6f0b278d8d5210f57465b8bd473681ae3
Choego155 <cedillolara153@gmail.com>
2026-09-20 20:48:56 -0600

    feat: initialize SQLite database on app startup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,12 +5,27 @@ import { useColorScheme } from 'react-native';
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
 
+import { initializeDatabase } from '@/data/database/database';
+
 SplashScreen.preventAutoHideAsync();
+
+/*
+ * Inicializamos la base de datos antes de que
+ * las pantallas de la aplicación se rendericen.
+ *
+ * CREATE TABLE IF NOT EXISTS hace que podamos
+ * ejecutar esto cada vez que inicia la app
+ * sin borrar información existente.
+ */
+initializeDatabase();
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider
+      value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+    >
       <AnimatedSplashOverlay />
       <AppTabs />
     </ThemeProvider>
